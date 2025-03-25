34e06eb604c91dc46ccc79b05f80104093398655
Nithish Aathreya <121860668+Nithish-Aathreya@users.noreply.github.com>
2025-03-25 23:53:43 +0530

    verify.v

--- verify.v
+++ verify.v
@@ -1,53 +1,62 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 30.10.2024 16:33:54
-// Design Name: 
-// Module Name: verify
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module verify();
-reg coin_in;
-wire [3:0]refund;
-wire [3:0] push;
-reg [3:0]coin_value;
+module vending_tb;
 
+  
+    logic clk, reset, coin_in;
+    logic [3:0] coin_value;
+    logic [3:0] refund, push;
+    
+    
+    vending uut (
+        .clk(clk),
+        .reset(reset),
+        .coin_in(coin_in),
+        .coin_value(coin_value),
+        .refund(refund),
+        .push(push)
+    );
+    
+   
+    always #5 clk = ~clk; 
+    
+    initial begin
+        $dumpfile("vending_tb.vcd");
+        $dumpvars(0, vending_tb);
 
-reg [1:0] state;
-
-
-vending uut(coin_in,refund,push,coin_value);
-
-initial
-begin
-$monitor("coin_in=%b,refund=%b,push=%b,coin_value=%b",coin_in,refund,push,coin_value);
-coin_in = 1'b0;
-
-forever
-#5 coin_in = ~coin_in;
-end
-
-initial
-begin
-#10 coin_value = 4'b1010;
-#20 coin_value = 4'b1000;
-
-#200 $finish;
-
-
-end
+        
+        clk = 0;
+        reset = 1;
+        coin_in = 0;
+        coin_value = 0;
+        #10;
+        
+        reset = 0;
+      
+        #10 coin_in = 1; coin_value = 4'b0101; // Insert 5
+        #10 coin_in = 0;
+        #20;
+        
+        
+        #10 coin_in = 1; coin_value = 4'b0111; // Insert 7
+        #10 coin_in = 0;
+        #20;
+        
+       
+        #10 coin_in = 1; coin_value = 4'b0011; // Insert 3
+        #10 coin_in = 0;
+        #20;
+        
+       
+        #10 coin_in = 1; coin_value = 4'b0010; // Insert 2
+        #10 coin_in = 0;
+        #10 coin_in = 1; coin_value = 4'b0011; // Insert 3
+        #10 coin_in = 0;
+        #20;
+        
+        
+        $finish;
+    end
+    
 endmodule
+
